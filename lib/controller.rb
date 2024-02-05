76ec8bfd9e2b0696fa133fb65264461fd78350ec
Korblen <malo.bastianelli@gmail.com>
2024-02-05 23:55:34 +0100

    presquefini

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,13 +1,35 @@
 require 'gossip'
 class ApplicationController < Sinatra::Base
+
   get '/' do
-      erb :index
+    erb :index, locals: {gossips: Gossip.all}
   end
+
   get '/gossips/new/' do
     erb :new_gossip
   end
+
   post '/gossips/new/' do
-    Gossip.new.save
+    gossip = Gossip.new(params["gossip_author"], params["gossip_content"])
+    gossip.save
     redirect to('/')
   end
+
+  get '/gossips/:id' do
+
+    id = params['id']
+
+    gossip = Gossip.find(id)
+  
+    if gossip
+        erb :show, locals: { gossip: gossip, gossip_id: id }
+    else
+        "Aucun potin trouvé avec l'ID #{id}"
+    end
+  end
+
+  get '/gossips/:id/?' do
+    redirect to("/gossips/#{params['id']}") 
+  end
+
 end
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -1,8 +1,50 @@
 require 'csv'
+require 'pry'
 class Gossip
+    attr_accessor :author, :content
+
+    def initialize(author, content)
+        @author = author
+        @content = content
+    end
+
     def save
         CSV.open("./db/gossip.csv", "ab") do |csv|
-            csv << ["Mon super auteur", "Ma super description"]
+            csv << [@author, @content]
         end
     end
+
+    def self.all
+        all_gossips = []
+        CSV.read("./db/gossip.csv").each do |csv_line|
+          all_gossips << Gossip.new(csv_line[0], csv_line[1])
+        end
+        return all_gossips
+    end
+
+    # def self.find(id)
+    #     # Convertir l'id en entier pour s'assurer de la comparaison numérique
+    #     id = id.to_i
+    #     liste = Gossip.all
+    #     liste.each_with_index do |element, index|
+    #         # Adjust the index to match the ID starting from 1
+    #         if index + 1 == id
+    #             return element
+    #             puts element
+    #         end
+    #     end
+    #     return nil
+    # end
+
+    def self.find(id)
+        id = id.to_i  # Convertir l'ID en entier pour la comparaison
+        CSV.foreach("./db/gossip.csv", headers: true).with_index(1) do |row, index|
+          if index == id
+            return Gossip.new(row['author'], row['content'])
+          end
+        end
+        return nil  # Retourne nil si aucun potin n'est trouvé avec cet ID
+    end
+
+
 end
