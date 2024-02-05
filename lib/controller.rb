9fce3f08c06b2fd20068fd26567ab73ed759752a
Korblen <malo.bastianelli@gmail.com>
2024-02-05 18:28:34 +0100

    2.4

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,5 +1,13 @@
+require 'gossip'
 class ApplicationController < Sinatra::Base
-    get '/' do
-        erb :index
+  get '/' do
+      erb :index
+  end
+  get '/gossips/new/' do
+    erb :new_gossip
+  end
+  post '/gossips/new/' do
+    Gossip.new.save
+    redirect to('/')
   end
 end
